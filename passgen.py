684b9d1cb64e76703ff76666c72403bea955005d
Robilotte <141377385+Robilotte@users.noreply.github.com>
2024-07-21 19:48:23 -0300

    Add files via upload

---
+++ passgen.py
@@ -0,0 +1,14 @@
+import string 
+import random
+
+longitud = int(input("Escribe el tamaño de la contraseña: "))
+
+caracteres = string.ascii_letters + string.digits + string.punctuation
+
+contrasena = "".join(random.choice(caracteres) for i in range(longitud))
+
+print("La contraseña generada es: " + contrasena)
+
+
+
+

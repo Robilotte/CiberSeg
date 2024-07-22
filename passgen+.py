b37ba44945276277b140050fbfc5e2055d9cc6ef
Robilotte <141377385+Robilotte@users.noreply.github.com>
2024-07-22 19:29:09 -0300

    Add files via upload

---
+++ passgen+.py
@@ -0,0 +1,51 @@
+import string 
+import random
+
+generador = int(input("Elige de que manera quieres crear la contraseña: \n 1- Letras, numeros y signos \n 2-Letras y numeros \n 3-Solo Letras \n 4-Solo Numeros \n 5-Salir \n"  ))
+
+while generador != 0:
+
+    if generador == 1:
+
+        longitud = int(input("Escribe el tamaño de la contraseña: "))
+
+        caracteres = string.ascii_letters + string.digits + string.punctuation
+
+        contrasena = "".join(random.choice(caracteres) for i in range(longitud))
+
+        print("La contraseña generada es: " + contrasena)
+
+    elif generador == 2:
+
+        longitud = int(input("Escribe el tamaño de la contraseña: "))
+
+        caracteres = string.ascii_letters + string.digits 
+
+        contrasena = "".join(random.choice(caracteres) for i in range(longitud))
+
+        print("La contraseña generada es: " + contrasena)
+
+    elif generador == 3:
+
+        longitud = int(input("Escribe el tamaño de la contraseña: "))
+
+        caracteres = string.ascii_letters 
+
+        contrasena = "".join(random.choice(caracteres) for i in range(longitud))
+
+        print("La contraseña generada es: " + contrasena)
+
+    elif generador == 4:
+
+        longitud = int(input("Escribe el tamaño de la contraseña: "))
+
+        caracteres = string.digits 
+
+        contrasena = "".join(random.choice(caracteres) for i in range(longitud))
+
+        print("La contraseña generada es: " + contrasena)
+
+    elif generador == 5:
+
+        print("Adios!")
+        break 
